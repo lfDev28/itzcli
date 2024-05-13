033d922b3fe5d1e41d6de30d6f1cf6b75d1dda8b
lfDev28 <lee.forster28@gmail.com>
2024-05-13 15:06:07 +1000

    Changed CI script to target new repo

--- scripts/deploy_public.sh
+++ scripts/deploy_public.sh
@@ -1,6 +1,3 @@
-# this script is intended to be used in a travis deploy operation as a way to create a release in a secondary github repo that is public
-# this functionality should not be needed if we move the itzcli repo to public or find a better toolchain function that accomplishes the same thing
-
 # these come from .travis.yml script cmd
 TOKEN=$1
 TAG=$2
@@ -11,7 +8,7 @@ GENERATE_POST_BODY() {
   cat <<EOF
 {
   "tag_name": "$TAG",
-  "target_commitish": "master",
+  "target_commitish": "main",
   "name": "$TAG",
   "body": "release version: $TAG",
   "draft": false,
@@ -27,7 +24,7 @@ API_RESPONSE_STATUS=$(
     --header 'Content-Type: application/json' \
     --data "$(GENERATE_POST_BODY)" \
     -s \
-    https://api.github.com/repos/cloud-native-toolkit/itzcli-release/releases
+    https://api.github.com/repos/lfDev28/itzcli/releases
 )
 echo API_RESPONSE:
 echo "$API_RESPONSE_STATUS"
@@ -38,8 +35,8 @@ BIN_DARWIN="itzcli-darwin-amd64.tar.gz"
 BIN_LINUX="itzcli-linux-amd64.tar.gz"
 
 # Construct url
-GH_ASSET_DARWIN="https://uploads.github.com/repos/cloud-native-toolkit/itzcli-release/releases/$RELEASE_ID/assets?name=$(basename $BIN_DARWIN)"
-GH_ASSET_LINUX="https://uploads.github.com/repos/cloud-native-toolkit/itzcli-release/releases/$RELEASE_ID/assets?name=$(basename $BIN_LINUX)"
+GH_ASSET_DARWIN="https://uploads.github.com/repos/lfDev28/itzcli/releases/$RELEASE_ID/assets?name=$(basename $BIN_DARWIN)"
+GH_ASSET_LINUX="https://uploads.github.com/repos/lfDev28/itzcli/releases/$RELEASE_ID/assets?name=$(basename $BIN_LINUX)"
 
 curl --data-binary @"$BIN_DARWIN" -H "Authorization: token $TOKEN" -H "Content-Type: application/octet-stream" $GH_ASSET_DARWIN
 curl --data-binary @"$BIN_LINUX" -H "Authorization: token $TOKEN" -H "Content-Type: application/octet-stream" $GH_ASSET_LINUX
